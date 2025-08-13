547501558514f127ca4b450ed9c949f4f2405599
szks-repo <szkscookie@gmail.com>
2025-08-14 07:47:22 +0900

    tidy: remove cobra comments

--- cmd/userClient.go
+++ cmd/userClient.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -27,14 +23,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(userClientCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// userClientCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// userClientCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
