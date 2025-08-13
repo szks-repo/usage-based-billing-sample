f1562eabf9834ee7e4eee88b7e60a34877edb785
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-08-14 07:47:22 +0900

    tidy: remove cobra comments

--- cmd/providerApi.go
+++ cmd/providerApi.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -30,14 +27,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(providerApiCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// providerApiCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// providerApiCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
