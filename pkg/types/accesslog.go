67ff9b14c5a6ca2e69ea38ebee3f3e824d29f5b4
szks-repo <szkscookie@gmail.com>
2025-08-15 23:46:36 +0900

    tidy: delete parquet tag

--- pkg/types/accesslog.go
+++ pkg/types/accesslog.go
@@ -3,12 +3,12 @@ package types
 import "time"
 
 type AccessLog struct {
-	Timestamp  time.Time `json:"timestamp" parquet:"name=timestamp, type=TIMESTAMP_MILLIS"`
-	ClientIP   string    `json:"client_ip" parquet:"name=client_ip, type=UTF8"`
-	Path       string    `json:"path" parquet:"name=path, type=UTF8"`
-	Method     string    `json:"method" parquet:"name=method, type=UTF8"`
-	Protocol   string    `json:"protocol" parquet:"name=protocol, type=UTF8"`
-	StatusCode int       `json:"status_code" parquet:"name=status_code, type=INT32"`
-	Latency    int64     `json:"latency" parquet:"name=latency_ms, type=INT64"`
-	UserAgent  string    `json:"user_agent" parquet:"name=user_agent, type=UTF8"`
+	Timestamp  time.Time `json:"timestamp"`
+	ClientIP   string    `json:"client_ip"`
+	Path       string    `json:"path"`
+	Method     string    `json:"method"`
+	Protocol   string    `json:"protocol"`
+	StatusCode int       `json:"status_code"`
+	Latency    int64     `json:"latency"`
+	UserAgent  string    `json:"user_agent"`
 }
