b0a413d331c4effdff842bc763b8b990bb2b8d01
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-08-17 00:28:50 +0900

    fix: sql

--- pkg/db/migration/000002_create_active_api_key.up.sql
+++ pkg/db/migration/000002_create_active_api_key.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `active_api_key` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `account_id` int(11) NOT NULL DEFAULT 0,
+    `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+    `account_id` bigint UNSIGNED NOT NULL DEFAULT 0,
     `api_key` VARCHAR(64) NOT NULL,
     `expired_at` DATETIME NOT NULL,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
