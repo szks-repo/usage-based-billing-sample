25a92ecd870a20aa56e2845c2b6d6b7c2a0509a6
szks-repo <szkscookie@gmail.com>
2025-08-16 18:58:22 +0900

    add: migration

---
+++ pkg/db/migration/000002_create_active_api_key.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `active_api_key` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `account_id` int(11) NOT NULL DEFAULT 0,
+    `api_key` VARCHAR(64) NOT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX (`api_key`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
