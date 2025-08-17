78a99ee01ddbba0523008c097e3778eefda6d370
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-08-17 13:26:08 +0900

    add: simulate api user

--- cmd/userClient.go
+++ cmd/userClient.go
@@ -1,9 +1,17 @@
 package cmd
 
 import (
-	"fmt"
+	"io"
+	"log/slog"
+	"math/rand/v2"
+	"net/http"
+	"sync"
+	"time"
 
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
+	"github.com/szks-repo/usage-based-billing-sample/pkg/db"
+	"github.com/szks-repo/usage-based-billing-sample/pkg/now"
 )
 
 // userClientCmd represents the userClient command
@@ -17,7 +25,76 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("userClient called")
+		slog.Info("Starting user client")
+
+		ctx := cmd.Context()
+
+		db.MustInit()
+		defer db.Close()
+
+		dbConn := db.Get()
+
+		rows, err := dbConn.QueryContext(ctx, `SELECT api_key FROM active_api_key WHERE expired_at > ?`, now.FromContext(ctx))
+		if err != nil {
+			panic(err)
+		}
+		defer rows.Close()
+
+		var apiKeys []string
+		for rows.Next() {
+			var s string
+			if err := rows.Scan(&s); err != nil {
+				panic(err)
+			}
+			apiKeys = append(apiKeys, s)
+		}
+
+		if len(apiKeys) == 0 {
+			slog.Info("Active api keys not found")
+			return
+		}
+
+		var (
+			apiUrl     = "http://localhost:8080/api/v1/one"
+			httpClient = &http.Client{
+				Transport: &http.Transport{
+					MaxIdleConnsPerHost: 30,
+					IdleConnTimeout:     time.Minute,
+				},
+			}
+		)
+
+		var wg sync.WaitGroup
+		for _, key := range apiKeys {
+			wg.Go(func() {
+				slog.Info("Start api user", "apiKey", key)
+				for {
+					select {
+					case <-ctx.Done():
+						return
+					default:
+					}
+
+					req := lo.Must(http.NewRequestWithContext(ctx, http.MethodGet, apiUrl, nil))
+					req.Header.Set("x-api-key", key)
+					res, err := httpClient.Do(req)
+					if err != nil {
+						slog.Info("Failed to *http.Client.Do", "error", err)
+						return
+					}
+					defer res.Body.Close()
+
+					if res.StatusCode != 200 {
+						slog.Warn("Http request status not 200", "statusCode", res.StatusCode)
+					} else {
+						io.Copy(io.Discard, res.Body)
+					}
+
+					time.Sleep(time.Microsecond * time.Duration(rand.IntN(500)))
+				}
+			})
+		}
+		wg.Wait()
 	},
 }
 
