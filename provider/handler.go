dfc4b4e4baa110d71bfae91639baef7844c2a1bb
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-08-18 00:21:08 +0900

    delete: handler log

--- provider/handler.go
+++ provider/handler.go
@@ -1,7 +1,6 @@
 package provider
 
 import (
-	"log/slog"
 	"math/rand/v2"
 	"net/http"
 	"time"
@@ -15,16 +14,12 @@ func NewApiHandler() *ApiHandler {
 }
 
 func (h *ApiHandler) HandleHelth(w http.ResponseWriter, r *http.Request) {
-	slog.Info("Handling health check request")
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"status": "healthy"}`))
 }
 
 func (h *ApiHandler) HandleApi1(w http.ResponseWriter, r *http.Request) {
-	slog.Info("Handling API 1 request")
-
 	time.Sleep(time.Millisecond * time.Duration(rand.IntN(50)))
 
 	w.WriteHeader(http.StatusOK)
@@ -32,8 +27,6 @@ func (h *ApiHandler) HandleApi1(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *ApiHandler) HandleApi2(w http.ResponseWriter, r *http.Request) {
-	slog.Info("Handling API 2 request")
-
 	time.Sleep(time.Millisecond * time.Duration(rand.IntN(200)))
 
 	w.WriteHeader(http.StatusOK)
