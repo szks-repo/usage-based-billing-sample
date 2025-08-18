71972b1e234617c2671f84772f7f7df2c9c7ce61
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-08-19 00:17:52 +0900

    add: invoice impl

--- invoice/reconciler.go
+++ invoice/reconciler.go
@@ -1,3 +1,25 @@
 package invoice
 
-type Reconciler struct{}
+import (
+	"context"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type UsageReconciler interface {
+	Do(ctx context.Context, baseDate time.Time, accountId int64) error
+}
+
+func NewUsageReconciler() UsageReconciler {
+	return &usageReconciler{}
+}
+
+type usageReconciler struct {
+	s3Client *s3.Client
+}
+
+func (rr *usageReconciler) Do(ctx context.Context, baseDate time.Time, accountId int64) error {
+	// todo
+	return nil
+}
