7b0d128772b333d44db403f1e081b801efef51f6
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-08-19 19:41:10 +0900

    tidy: ResponseWriterWrapper move pkg, gofmt

--- provider/middleware.go
+++ provider/middleware.go
@@ -10,6 +10,7 @@ import (
 	"github.com/cenkalti/backoff/v4"
 	"github.com/streadway/amqp"
 
+	httplib "github.com/szks-repo/usage-based-billing-sample/pkg/http"
 	"github.com/szks-repo/usage-based-billing-sample/pkg/now"
 	"github.com/szks-repo/usage-based-billing-sample/pkg/rabbitmq"
 	"github.com/szks-repo/usage-based-billing-sample/pkg/types"
@@ -54,14 +55,11 @@ func (mw *middleware) Wrap(next http.Handler) http.Handler {
 		ctx := context.WithValue(r.Context(), ctxkey.ApiKey{}, apiKey)
 		ctx = context.WithValue(ctx, ctxkey.AccountId{}, accountId)
 
-		w2 := &ResponseWriterWrapper{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
+		w2 := httplib.NewResponseWriterWrapper(w)
 
 		next.ServeHTTP(w2, r.WithContext(ctx))
-		slog.Info("End main handler", "path", r.URL.Path, "satusCode", w2.statusCode)
-		if w2.statusCode < 200 || w2.statusCode >= 300 {
+		slog.Info("End main handler", "path", r.URL.Path, "satusCode", w2.StatusCode())
+		if w2.StatusCode() < 200 || w2.StatusCode() >= 300 {
 			return
 		}
 
@@ -72,7 +70,7 @@ func (mw *middleware) Wrap(next http.Handler) http.Handler {
 			ClientIP:   r.RemoteAddr,
 			Path:       r.URL.Path,
 			Method:     r.Method,
-			StatusCode: w2.statusCode,
+			StatusCode: w2.StatusCode(),
 			Latency:    int64(time.Since(start)),
 			UserAgent:  r.UserAgent(),
 		})
@@ -101,23 +99,5 @@ func (mw *middleware) Wrap(next http.Handler) http.Handler {
 			slog.Error("Failed to publish message to RabbitMQ", "error", err)
 			return
 		}
-
 	})
-}
-
-type ResponseWriterWrapper struct {
-	http.ResponseWriter
-	statusCode   int
-	bytesWritten int
-}
-
-func (w *ResponseWriterWrapper) WriteHeader(statusCode int) {
-	w.statusCode = statusCode
-	w.ResponseWriter.WriteHeader(statusCode)
-}
-
-func (w *ResponseWriterWrapper) Write(data []byte) (int, error) {
-	n, err := w.ResponseWriter.Write(data)
-	w.bytesWritten += n
-	return n, err
 }
